76282ebad87558c926df981ac4e1f7b7f942ce9d
simonk7719 <simonk7719@gmail.com>
2016-09-02 10:58:33 -0700

    Added Files

---
+++ APLesson1/Lab 01/IsaacGelman.java
@@ -0,0 +1,18 @@
+public class IsaacGelman
+{
+	public static void main(String[]args)
+	{
+		System.out.println("      @@@@@@@@@             ");
+		System.out.println("     @@@@@@@@@@           ");
+		System.out.println("    @                            @           ");
+		System.out.println("     @  @@   U  @@   @           ");
+		System.out.println("      @    *>    /   *>   @            ");
+		System.out.println("      @         /           @             ");
+		System.out.println("        !        ^^         !              ");
+		System.out.println("          !     ~~       /               ");
+		System.out.println("            !            !     ");
+		System.out.println("              ---I----                ");
+		System.out.println("                                ");
+		System.out.println("                                ");
+	}
+}
---
+++ APLesson1/Lab 01/MikeyHa.java
@@ -0,0 +1,18 @@
+public class MikeyHa
+{
+	public static void main(String[]args)
+	{
+		System.out.println("        /////////////////                 ");
+		System.out.println("      //////////////////////              ");
+		System.out.println("      ///    /// ////       //        ");
+		System.out.println("       //     /           /    ");
+		System.out.println("       /----I  I---I  I---/");
+		System.out.println("       /     /             /      ");
+		System.out.println("       /    /             /      ");
+		System.out.println("       /    U             /                     ");
+		System.out.println("        /   ~   ~       /     ");
+		System.out.println("        /    uu       /               ");
+		System.out.println("           ---------                     ");
+		System.out.println("                            ");
+	}
+}
