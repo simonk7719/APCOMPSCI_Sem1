834e42b27ddc56fc4a888c0b667c35cd3e4b7693
simonk7719 <simonk7719@gmail.com>
2016-09-09 10:46:20 -0700

    commit

---
+++ Lab 02/Complex_Calculation.java
@@ -0,0 +1,14 @@
+public class Complex_Calculation
+{
+	public static void main(String[]args)
+	{
+		int l = 8;
+		int w = 5;
+		int h = 3;
+		int value = 2 * l * w + 2 * w * h + 2 * h * l;
+		
+		System.out.println("The surface area of a retangular prism is " + value);
+		
+		
+	}
+}
---
+++ Lab 02/Concatenation.java
@@ -0,0 +1,14 @@
+public class Concatenation
+{
+	public static void main(String[]args)
+	{
+		String name = "Simon Kim";
+		String str = "13434 Banyan Way";
+		String city = "San Diego, CA";
+		int zip = 92130;
+		
+		System.out.println(name + "\n" + str + "\n" + city + "\n" + zip);
+		
+		
+	}
+}
