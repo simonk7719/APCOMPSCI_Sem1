43044d5de14f451d1ac7bf8b86b996180c121904
simonk7719 <simonk7719@gmail.com>
2016-10-05 12:04:53 -0700

    4.2 added

--- Lab_04.3/Circle.java
+++ Lab_04.3/Circle.java
@@ -3,10 +3,17 @@ public class Circle
 {
 	public static void main(String[]args)
 	{
-	
+		Circle x = new Circle();
+		
 		Scanner kb = new Scanner(System.in);
 		System.out.println("What is the radius of your circle?");
 		double r = kb.nextDouble();
+
+		x.average(r);
+	}
+
+	public void average(double r)
+	{
 		double a = r * r * 3.14159;
 		System.out.printf("the area of your circle with " + r + " radius is " + "%100000.3f", a);
 	}
--- Lab_04.3/Rectangle.java
+++ Lab_04.3/Rectangle.java
@@ -3,6 +3,8 @@ public class Rectangle
 {
 	public static void main(String[]args)
 	{
+		Rectangle box = new Rectangle();
+		
 		Scanner kb = new Scanner(System.in);
 		System.out.println("What is the height of the Rectangle?");
 		double h = kb.nextDouble();
@@ -11,11 +13,16 @@ public class Rectangle
 		System.out.println("What is the width of the Rectangle?");
 		double w = kb.nextDouble();
 		
+		box.calcperim(h,l,w);
+	}
+
+	public void calcperim(double h, double l, double w)
+	{
 		double a = 4 * h ;
 		double b = 4 * l ;
 		double c = 4 * w ;
 		double p = a + b + c;
-		System.out.printf("Your Rectangle's perimeter is %10000.5f ft around.", p);
+		System.out.printf("Your Rectangle's perimeter is " + "%10000.5f", p + " ft around.");
 	}
 	
 }
