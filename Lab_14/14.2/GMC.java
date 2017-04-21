5a39fdcc1a0536e30da760504e7a66a8ef2e7c79
simonk7719 <simonk7719@gmail.com>
2017-04-21 10:51:44 -0700

    fix

--- Lab_14/14.2/GMC.java
+++ Lab_14/14.2/GMC.java
@@ -1,13 +1,9 @@
 import java.util.*;
 public class GMC extends Car {
-<<<<<<< HEAD
-	public GMC(double x, double y){
-		super();
-		this.move(x,y);
-=======
+	
 	public GMC(double p, double q){
 		super();
 		this.move(p,q);
->>>>>>> origin/master
+
 	}
 }
--- Lab_14/14.2/Honda.java
+++ Lab_14/14.2/Honda.java
@@ -1,13 +1,7 @@
 import java.util.*;
 public class Honda extends Car {
-<<<<<<< HEAD
-	public Honda (double[] Pos){
-		super();
-		this.move(Pos[0],Pos[1]);
-=======
 	public Honda (double[] pos){
 		super();
 		this.move(pos[0],pos[1]);
->>>>>>> origin/master
 	}
 }
